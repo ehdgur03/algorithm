7d87b35317ce655860c9703562cd3797a5f28429
ehdgur03 <127325965+ehdgur03@users.noreply.github.com>
2023-05-14 20:21:00 +0900

    Add files via upload

---
+++ 10809.py
@@ -0,0 +1,4 @@
+x = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+S = input()
+for i in x:
+    print(S.find(i), end='')
